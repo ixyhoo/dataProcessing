3e1b62ed442e4a694b3ff6167321c53b4aa9fe1a
ixyhoo <ixyhoo@outlook.fr>
2023-05-23 21:33:24 +0200

    rangeValue

--- password-maker/index.js
+++ password-maker/index.js
@@ -1,1 +1,20 @@
-const
+
+const dataLowercase = "abcdefghijklmnopqrstuvwxyz";
+const dataUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const dataNumbers = "0123456789";
+const dataSymbols = "!@#$%^&*()_+=-{}[];':\"\\|,.<>?/~`";
+const rangeValue = document.getElementById("password-length");
+console.log(rangeValue.value);
+
+function generatePassword() {
+    let data = []
+    if (lowercase.checked) data.push(...dataLowercase);
+    if (uppercase.checked) data.push(...dataUppercase);
+    if (numbers.checked) data.push(...dataNumbers);
+    if (symbols.checked) data.push(...dataSymbols);
+
+    console.log(data[Math.floor(Math.random() * data.length)]);
+}
+
+generateButton.addEventListener("click", generatePassword);
+
