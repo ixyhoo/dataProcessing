d849208c2dc379f1102bcef91f4a08fad8c82176
ixyhoo <ixyhoo@outlook.fr>
2023-05-30 21:37:10 +0200

    email checker

--- form-checker/index.js
+++ form-checker/index.js
@@ -7,12 +7,39 @@ const inputs = document.querySelectorAll(
   'input[type="text"], input[type="password"]'
 );
 
+let pseudo, email, password, confirmPass;
+
+const errorDisplay = (tag, message, valid) => {
+  const container = document.querySelector("." + tag + "-container");
+  const span = document.querySelector("." + tag + "-container > span");
+
+  if (!valid) {
+    container.classList.add("error");
+    span.textContent = message;
+  } else {
+    container.classList.remove("error");
+    span.textContent = message;
+  }
+}
+
 const pseudoChecker = (value) => {
-  console.log(value);
+  if (value.length > 0 && (value.length < 3 || value.length > 20)) {
+    errorDisplay("pseudo", "The pseudo must contain between 3 and 20 characters.");
+    pseudo = null;
+  } else if (!/^[a-zA-Z0-9_.-]+$/.test(value)) {
+    errorDisplay("pseudo", "The pseudo must not contain special characters.");
+    pseudo = null;
+  } else {
+    errorDisplay("pseudo", "", true);
+    pseudo = value;
+    return true;}
 };
 
 const emailChecker = (value) => {
-  console.log(value);
+  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
+    errorDisplay("email", "Invalid email address.");
+    return false;
+  } else if
 };
 
 const passwordChecker = (value) => {
