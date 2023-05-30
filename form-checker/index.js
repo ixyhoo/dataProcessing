06fcc3058898bfbfd0bc11aa3d068bb7f578edea
ixyhoo <ixyhoo@outlook.fr>
2023-05-30 21:16:42 +0200

    add regex

--- form-checker/index.js
+++ form-checker/index.js
@@ -1,5 +1,45 @@
-let body = document.querySelector('body');
+let body = document.querySelector("body");
+document.body.addEventListener("click", function () {
+  body.classList.toggle("pink-gradient");
+});
 
-document.body.addEventListener('click', function () {
-  body.classList.toggle('pink-gradient');
+const inputs = document.querySelectorAll(
+  'input[type="text"], input[type="password"]'
+);
+
+const pseudoChecker = (value) => {
+  console.log(value);
+};
+
+const emailChecker = (value) => {
+  console.log(value);
+};
+
+const passwordChecker = (value) => {
+  console.log(value);
+};
+
+const confirmChecker = (value) => {
+  console.log(value);
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    switch (e.target.id) {
+      case "pseudo":
+        pseudoChecker(e.target.value);
+        break;
+      case "email":
+        emailChecker(e.target.value);
+        break;
+      case "password":
+        passwordChecker(e.target.value);
+        break;
+      case "confirm":
+        confirmChecker(e.target.value);
+        break;
+      default:
+        null;
+    }
+  });
 });
